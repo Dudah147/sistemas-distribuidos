44e32bc64e1a325661b250c4afc704ed8fa577c4
Maria Eduarda Macedo <mdudu2213@gmail.com>
2024-04-26 18:03:23 -0300

    feat: adicionado JPA de candidato

--- src/main/java/model/Candidato.java
+++ src/main/java/model/Candidato.java
@@ -4,10 +4,58 @@
  */
 package model;
 
+import jakarta.persistence.*;
+
 /**
  *
  * @author dudam
  */
+@Entity
 public class Candidato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCandidato;
+    private String email;
+    private String nome;
+    private int senha;
+
+    public int getIdCandidato() {
+        return idCandidato;
+    }
+
+    public void setIdCandidato(int idCandidato) {
+        this.idCandidato = idCandidato;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Candidato{" + "idCandidato=" + idCandidato + ", email=" + email + ", nome=" + nome + ", senha=" + senha + '}';
+    }
+    
     
 }
--- src/main/java/server/ServerApp.java
+++ src/main/java/server/ServerApp.java
@@ -4,17 +4,30 @@
  */
 package server;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import model.Candidato;
+
 /**
  *
  * @author dudam
  */
 public class ServerApp {
-
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("SistemasDistribuidos");
+    private static EntityManager em = emf.createEntityManager();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Candidato candidato = new Candidato();
+        candidato.setEmail("teste@teste.com");
+        candidato.setNome("teste");
+        candidato.setSenha(123);
+        
+        em.getTransaction().begin();
+        em.persist(candidato);
+        em.getTransaction().commit();
     }
     
 }
